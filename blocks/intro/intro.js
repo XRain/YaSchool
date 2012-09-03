961d6b3c5ba6078499eca3510f124693bee1a1aa
User <user@Glukoed.local>
2012-09-03 05:58:01 +0300

    Правка под браузеры

--- blocks/intro/intro.js
+++ blocks/intro/intro.js
@@ -1,6 +1,12 @@
 $(document).ready(function() {
+
+
+
+
+
     $('.bl-intro__switchDown').on('click', function() {
-        $(this).parent().parent().animate({'background-position-y': -50});
+        $(this).parent().parent().animate({'backgroundPosition': '0px -50px'});
         $(this).parent().animate({top: -500});
     });
 });
+
--- blocks/questions/questions.js
+++ blocks/questions/questions.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     $('.bl-questions__switchUp').on('click', function() {
-        $(this).parent().parent().animate({'background-position-y': 0});
+        $(this).parent().parent().animate({'backgroundPosition': '0px 0px'});
         $(this).parent().animate({top: 0});
     });
 });
